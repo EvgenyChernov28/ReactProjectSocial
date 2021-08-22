667807358dfa27f3317f6a92222b42b8b07ba9e9
EvgenyChernov28 <78750169+EvgenyChernov28@users.noreply.github.com>
2021-08-22 15:51:05 +0300

    M-UI pagination

--- src/components/common/pagination/Pagination.jsx
+++ src/components/common/pagination/Pagination.jsx
@@ -1,41 +1,23 @@
 import React, { useState } from "react";
 import styles from "./pagination.module.css";
+import Pagination from '@material-ui/lab/Pagination';
 
+let PaginationContainer = ({totalUsersCount, pageSize, currentPage, onPageChange}) => {
 
-let Pagination = ({totalUsersCount, pageSize, currentPage, onPageChange, portionSize = 10}) => {
     let pageCount = Math.ceil(totalUsersCount / pageSize);
 
-    let pages = [];
+    const [page, setPage] = useState(currentPage);
 
-    for (let i = 1; i <= pageCount; i++) {
-        pages.push(i);
-    }
+    const handleChange = (event, value) => {
+      setPage(value);
+      onPageChange(value)
+    };
 
-    let portionCount = Math.ceil(pageCount / portionSize);
-    let [portionNumber, setPortionNumber] = useState(1);
-    let leftPortionPageNumber = (portionNumber - 1)*portionSize + 1;
-    let rightPortionPageNumber = portionNumber * portionSize;
-    console.log(portionCount, portionNumber, leftPortionPageNumber, rightPortionPageNumber);
     return (
         <div className={styles.pagination}>
-            {portionNumber > 1 && <button onClick={() => {setPortionNumber(portionNumber - 1); onPageChange(portionNumber)}}>Пред.</button>}
-            {pages
-                .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
-                .map((p) => {
-                return (
-                    <span key={p}
-                        className={currentPage === p && styles.selectedPage}
-                        onClick={(e) => {
-                            onPageChange(p);
-                        }}
-                    >
-                        {p}
-                    </span>
-                );
-            })}
-            {portionCount > portionNumber && <button onClick={() => {setPortionNumber(portionNumber + 1); onPageChange(portionNumber)}}>След.</button>}
+            <Pagination variant="outlined" count={pageCount} boundaryCount={2} siblingCount={2} page={page} onChange={handleChange} color="secondary"/>
         </div>
     );
 };
 
-export default Pagination;
+export default PaginationContainer;
