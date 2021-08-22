badba9c6d4d4d47d5c740ed514255a6ed26e3530
EvgenyChernov28 <78750169+EvgenyChernov28@users.noreply.github.com>
2021-08-22 18:02:00 +0300

    M-UI Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,60 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
 import { maxLengthCreator, required } from "../../utils/validators";
 import { Input } from "../common/Form/FormsControls";
 import { login } from "../../redux/auth_reducer";
 import { Redirect } from "react-router-dom";
-import styles from "../common/Form/FormsControls.module.css"
+import styles from "../common/Form/FormsControls.module.css";
+import SignIn from "./signin";
 
 const Login = (props) => {
+    const [choseLoginPage, SetLoginPage] = useState(false)
 
     const onSubmit = (formData) => {
-        props.login(formData.email, formData.password, formData.rememberMe, formData.captcha)
+        props.login(formData.email, formData.password, formData.rememberMe, formData.captcha);
+        console.log(formData.email, formData.password, formData.rememberMe, formData.captcha);
+    };
+    if (props.isAuth) {
+        return <Redirect to={"/profile"} />;
     }
-    if(props.isAuth){
-        return <Redirect to={"/profile"} />
+    const changeLoginPage = () =>{
+        SetLoginPage(!choseLoginPage)
     }
+
     return (
         <div>
-            <h1>Login</h1>
-            <LoginReduxForm captchUrl={props.captchUrl} onSubmit={onSubmit}/>
+            <button onClick={changeLoginPage} >Переключатель страницы авторизации</button>
+            {choseLoginPage ? <SignIn onSubmit={onSubmit} /> : <LoginReduxForm captchUrl={props.captchUrl} onSubmit={onSubmit} />}
         </div>
     );
 };
 
-const maxLength20 = maxLengthCreator(40)
+const maxLength20 = maxLengthCreator(40);
 const LoginForm = (props) => {
     return (
-        <form onSubmit={props.handleSubmit}>
-            <div>
-                <Field placeholder={"логин"} name={"email"} component={Input} validate={[required, maxLength20]}/>
-            </div>
-            <div>
-                <Field placeholder={"пароль"} name={"password"} component={Input} validate={[required, maxLength20]} type={"password"}/>
-            </div>
-            <div>
-                <Field type={"checkbox"} name={"rememberMe"} component={Input} validate={[required, maxLength20]}/> Запомнить меня
-            </div>
-            {props.error && <div className={styles.form_summary_error}>
-                {props.error}
-            </div>}
-            {props.captchUrl && <img src={props.captchUrl}/>}
-            {props.captchUrl && <Field placeholder={"Введите символы с картинки"} name={"captcha"} component={Input} validate={[required]}/>}
-            <div>
-                <button>Нажать</button>
-            </div>
-        </form>
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={props.handleSubmit}>
+                <div>
+                    <Field placeholder={"логин"} name={"email"} component={Input} validate={[required, maxLength20]} />
+                </div>
+                <div>
+                    <Field
+                        placeholder={"пароль"}
+                        name={"password"}
+                        component={Input}
+                        validate={[required, maxLength20]}
+                        type={"password"}
+                    />
+                </div>
+                <div>
+                    <Field type={"checkbox"} name={"rememberMe"} component={Input} validate={[required, maxLength20]} />{" "}
+                    Запомнить меня
+                </div>
+                {props.error && <div className={styles.form_summary_error}>{props.error}</div>}
+                {props.captchUrl && <img src={props.captchUrl} />}
+                {props.captchUrl && (
+                    <Field
+                        placeholder={"Введите символы с картинки"}
+                        name={"captcha"}
+                        component={Input}
+                        validate={[required]}
+                    />
+                )}
+                <div>
+                    <button>Нажать</button>
+                </div>
+            </form>
+        </div>
     );
-}; 
+};
 
 const LoginReduxForm = reduxForm({
     form: "login",
-})(LoginForm)
+})(LoginForm);
 
 const mapStateToProps = (state) => ({
     captchUrl: state.auth.captcha,
-    isAuth: state.auth.isAuth
-})
+    isAuth: state.auth.isAuth,
+});
 
-export default connect(mapStateToProps, {login})(Login);
+export default connect(mapStateToProps, { login })(Login);
---
+++ src/components/Login/signin.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: "100%",
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function SignIn(props) {
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleClick = () => {
+      console.log(email, password);
+    };
+    const classes = useStyles();
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}></Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <form className={classes.form} noValidate onSubmit={props.onSubmit}>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        onChange={(event) => {setEmail(event.target.value)}}
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        onChange={(event) => {setPassword(event.target.value)}}
+                    />
+                    <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+                    <Button onClick={() => props.onSubmit({email, password})}  fullWidth variant="contained" color="primary" className={classes.submit}>
+                        Sign In
+                    </Button>
+                </form>
+            </div>
+        </Container>
+    );
+}
